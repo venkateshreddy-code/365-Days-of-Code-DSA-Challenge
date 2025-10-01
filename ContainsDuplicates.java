2b8474f870e1739094fdc11eef19f1f88a340d3f
venkatesh reddy Ningam <178813027+venkateshreddy-code@users.noreply.github.com>
2025-10-01 10:34:57 -0400

    Update ContainsDuplicates.java

--- ContainsDuplicates.java
+++ ContainsDuplicates.java
@@ -1,16 +1,23 @@
 import java.util.HashSet;
 
-public class Solution {
+public class ContainsDuplicate {
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
-
+        
         for (int num : nums) {
             if (set.contains(num)) {
-                return true;
+                return true; // duplicate found
             }
             set.add(num);
         }
+        return false; // no duplicates
+    }
 
-        return false; // only return false after checking all numbers
+    public static void main(String[] args) {
+        ContainsDuplicate cd = new ContainsDuplicate();
+        int[] arr1 = {2, 3, 4, 2};
+        int[] arr2 = {6, 7, 8, 9};
+        System.out.println(cd.containsDuplicate(arr1)); // true
+        System.out.println(cd.containsDuplicate(arr2)); // false
     }
 }
